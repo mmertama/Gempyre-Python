1dc421b6f50147564620580fa822af9e4de87b73
Markus Mertama <mertama@gmail.com>
2020-03-22 23:07:16 +0200

    fix GIL issues

--- telex_python.cpp
+++ telex_python.cpp
@@ -8,6 +8,31 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(Telex, m) {
     m.def("setDebug", &Telex::setDebug);
+    m.def("version", &Telex::version);
+    py::class_<Telex::Event>(m, "Event")
+            .def_readonly("element", &Telex::Event::element)
+            .def_readonly("properties", &Telex::Event::properties)
+            ;
+    py::class_<Telex::Element>(m, "Element")
+            .def(py::init<const Telex::Element&>())
+            .def(py::init<Telex::Ui&, const std::string&>())
+            .def(py::init<Telex::Ui&, const std::string, const std::string&, const Telex::Element&>())
+            .def("ui", py::overload_cast<>(&Telex::Element::ui, py::const_))
+            .def("ui", py::overload_cast<>(&Telex::Element::ui))
+            .def("id", &Telex::Element::id)
+            .def("subscribe", [](Telex::Element* el, const std::string& name, std::function<void(const Telex::Event& ev)> handler, const std::vector<std::string>& properties, const std::chrono::milliseconds& throttle = 0ms) {
+                 return el->subscribe(name, [handler](const Telex::Event& ev){py::gil_scoped_acquire acquire; handler(ev);}, properties, throttle);
+                }, py::arg("name"), py::arg("handler"), py::arg("properties") = std::vector<std::string>{}, py::arg("throttle") = 0ms)
+            .def("setHTML", &Telex::Element::setHTML)
+            .def("setAttribute", &Telex::Element::setAttribute)
+            .def("attributes", &Telex::Element::attributes)
+            .def("children", &Telex::Element::children)
+            .def("values", &Telex::Element::values)
+            .def("html", &Telex::Element::html)
+            .def("remove", &Telex::Element::remove)
+            .def("type", &Telex::Element::type)
+            .def("rect", &Telex::Element::rect)
+            ;
     py::class_<Telex::Ui>(m, "Ui")
        // Should I comment these out as using them is confusing due browser security concerns
         .def(py::init<const std::string&, const std::string&, const std::string&, unsigned short, const std::string& >(),
@@ -38,19 +63,41 @@ PYBIND11_MODULE(Telex, m) {
             )
         .def_readonly_static("UseDefaultPort", &Telex::Ui::UseDefaultPort)
         .def_readonly_static("UseDefaultRoot", &Telex::Ui::UseDefaultRoot)
-        .def("run", &Telex::Ui::run)
+        .def("run", &Telex::Ui::run, py::call_guard<py::gil_scoped_release>())
         .def("exit", &Telex::Ui::exit)
         .def("close", &Telex::Ui::close)
-        .def("onUiExit", &Telex::Ui::onUiExit)
-        .def("onReload", &Telex::Ui::onReload)
-        .def("onOpen", &Telex::Ui::onOpen)
-        .def("onError", &Telex::Ui::onError)
+        .def("onExit", [](Telex::Ui* ui, std::function<void ()> onExitFunction = nullptr)->Telex::Ui& {
+            return ui->onExit(onExitFunction ? [onExitFunction]() {py::gil_scoped_acquire acquire; return onExitFunction(); } :
+                static_cast<decltype(onExitFunction)>(nullptr)); })
+        .def("onReload", [](Telex::Ui* ui, std::function<void ()> onReloadFunction = nullptr)->Telex::Ui& {
+        return ui->onReload(onReloadFunction ? [onReloadFunction]() {py::gil_scoped_acquire acquire; return onReloadFunction(); } :
+            static_cast<decltype(onReloadFunction)>(nullptr)); })
+        .def("onOpen", [](Telex::Ui* ui, std::function<void ()> onOpenFunction = nullptr)->Telex::Ui& {
+        return ui->onOpen(onOpenFunction ? [onOpenFunction]() {py::gil_scoped_acquire acquire; return onOpenFunction(); } :
+            static_cast<decltype(onOpenFunction)>(nullptr)); })
+        .def("onError", [](Telex::Ui* ui, std::function<void (const std::string& element, const std::string& info)> onErrorFunction = nullptr)->Telex::Ui& {
+        return ui->onError(onErrorFunction ? [onErrorFunction](const std::string& element, const std::string& info) {py::gil_scoped_acquire acquire; return onErrorFunction(element, info); } :
+            static_cast<decltype(onErrorFunction)>(nullptr)); })
         .def("setLogging", &Telex::Ui::setLogging)
         .def("eval", &Telex::Ui::eval)
         .def("debug", &Telex::Ui::debug)
         .def("alert", &Telex::Ui::alert)
         .def("open", &Telex::Ui::open, py::arg("url"), py::arg("name") = "")
-        .def("startTimer", py::overload_cast<const std::chrono::milliseconds&, bool, const std::function<void ()>&>(&Telex::Ui::startTimer))
-        .def("startTimer", py::overload_cast<const std::chrono::milliseconds&, bool, const std::function<void (Telex::Ui::TimerId)>&>(&Telex::Ui::startTimer))
+        .def("startTimer", [](Telex::Ui* ui, const std::chrono::milliseconds& ms, bool b, const std::function<void ()>& f) {
+            return ui->startTimer(ms, b, [f](){py::gil_scoped_acquire acquire; f();});})
+        // When wrapping in fp (to enable GIL), there is no need: py::overload_cast<const std::chrono::milliseconds&, bool, const std::function<void (Telex::Ui::TimerId)>&>(&Telex::Ui::startTimer)
+        .def("startTimer", [](Telex::Ui* ui, const std::chrono::milliseconds& ms, bool b, const std::function<void (Telex::Ui::TimerId)>& f) {
+            return ui->startTimer(ms, b, [f](Telex::Ui::TimerId tid){py::gil_scoped_acquire acquire; f(tid);});})
+        .def("stopTimer", &Telex::Ui::stopTimer)
+        .def("root", &Telex::Ui::root)
+        .def("addressOf", &Telex::Ui::addressOf)
+        .def("byClass", &Telex::Ui::byClass)
+        .def("byName", &Telex::Ui::byName)
+        .def("ping", &Telex::Ui::ping)
+        .def("extension", &Telex::Ui::extension)
+        .def("resource", &Telex::Ui::resource)
+        .def("addFile", &Telex::Ui::addFile)
+        .def("beginBatch", &Telex::Ui::beginBatch)
+        .def("endBatch", &Telex::Ui::endBatch)
             ;
 }
--- test/telex_test.py
+++ test/telex_test.py
@@ -1,13 +1,51 @@
 import sys
 import os
 import Telex
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
 from Telex_utils import resource
 
-Telex.setDebug()
-name = sys.argv[1]
+#Telex.setDebug()
+name = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.path.dirname(sys.argv[0]), "python_test_1.html")
 map, names = resource.fromFile(name)
 print(names[name], name)
+
 ui = Telex.Ui(map, names[name])
-ui.onUiExit(lambda: print("on Exit"))
+
+ver, major, minor = Telex.version()
+Telex.Element(ui, 'ver').setHTML("Telex Version: " + str(ver) + '.' + str(major) + '.' + str(minor));
+
+def onStart():
+    print(ui.root().html())
+
+ui.onOpen(onStart)
+
+ui.startTimer(timedelta(seconds=1), False, lambda: Telex.Element(ui, 'time').setHTML(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
+elementCount = 0
+
+addbutton = Telex.Element(ui, 'do')
+def addElement(event):
+    global elementCount
+    new = Telex.Element(ui, "name_" + str(elementCount), "img", ui.root())
+    elementCount += 1
+    new.setAttribute("SRC", "https://www.animatedimages.org/data/media/202/animated-dog-image-0931.gif")
+
+    
+
+addbutton.subscribe('click', addElement)
+
+removebutton = Telex.Element(ui, 'take')
+
+def removeElement(event):
+    global elementCount
+    if elementCount <= 0:
+        return
+    elementCount -= 1
+    Telex.Element(ui, "name_" + str(elementCount)).remove()
+removebutton.subscribe('click', removeElement)
+
+ui.onExit(lambda: print("on Exit"))
 ui.run()
 
