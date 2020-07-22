a46adc021ff481fe083c08f931d4c689dc0b00d3
Markus Mertama <markus.mertama@intopalo.com>
2020-07-22 21:46:33 +0300

    small fixes to build files

--- build_osx.sh
+++ build_osx.sh
@@ -1,8 +1,9 @@
+cmake CMakeLists.txt
 cmake --build . --config Release
 OS_VER_LONG=$(defaults read loginwindow SystemVersionStampAsString)
-PY_VER_LONG=$(python --version) 
-OS_VER=10.15
-PY_VER=3.8
-mv Telex.so  build/lib.macosx-10.15-x86_64-3.8/Telex.cpython-38-darwin.so
-#mv *.so build/lib.macosx-${OS_VER}-x86_64-${PY_VER}/
+OS_VER=$(echo "${OS_VER_LONG}" | grep -o '[0-9]\+.[0-9]\+')
+PY_VER_LONG=$(python3 --version)
+PY_VER=$(echo "${PY_VER_LONG}" | grep -o '[0-9]\+.[0-9]\+')
+#mv Telex.so  build/lib.macosx-10.15-x86_64-3.8/Telex.cpython-38-darwin.so
+mv *.so build/lib.macosx-${OS_VER}-x86_64-${PY_VER}/
 pip3 install -e . --user
