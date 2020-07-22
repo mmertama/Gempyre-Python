1500b7455d777b9214945f2cc1bb40c34bde870f
Markus Mertama <markus.mertama@intopalo.com>
2020-07-22 17:23:02 +0300

    update build scripts

--- build_osx.sh
+++ build_osx.sh
@@ -1,3 +1,8 @@
 cmake --build . --config Release
-mv *.so build/lib.macosx-10.14-x86_64-3.7/
+OS_VER_LONG=$(defaults read loginwindow SystemVersionStampAsString)
+PY_VER_LONG=$(python --version) 
+OS_VER=10.15
+PY_VER=3.8
+mv Telex.so  build/lib.macosx-10.15-x86_64-3.8/Telex.cpython-38-darwin.so
+#mv *.so build/lib.macosx-${OS_VER}-x86_64-${PY_VER}/
 pip3 install -e . --user
