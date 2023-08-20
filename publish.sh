6c11cc498e0a4f05afd4006e11ce8772266615b3
Markus Mertama <mertama@gmail.com>
2023-08-20 19:34:23 +0300

    fix release name

--- publish.sh
+++ publish.sh
@@ -7,8 +7,12 @@ targets=( "Linux" "Windows" "MacOS" )
 rm -rf dist
 mkdir -p dist
 
+
 REL=$(curl -Ls -o /dev/null -w %{url_effective}  https://github.com/mmertama/Gempyre-Python/releases/latest | grep -o "[^/]*$")
 
 for value in "${targets[@]}"; do
-    echo "https://github.com/mmertama/Gempyre-Python/releases/download/$REL/$value.tar.gz"
+    wget "https://github.com/mmertama/Gempyre-Python/releases/download/$REL/$value.tar.gz"
+    tar -xzvf $value.tar.gz
+    rm $value.tar.gz
 done
+
