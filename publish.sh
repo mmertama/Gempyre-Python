beffab71ca78fae4e887949c3cba78cd2bf0983b
Markus Mertama <mertama@gmail.com>
2024-03-29 20:34:36 +0200

    update CI flow

--- publish.sh
+++ publish.sh
@@ -13,9 +13,10 @@ mkdir -p dist
 REL=$(curl -Ls -o /dev/null -w %{url_effective}  https://github.com/mmertama/Gempyre-Python/releases/latest | grep -o "[^/]*$")
 
 for value in "${targets[@]}"; do
-    wget "https://github.com/mmertama/Gempyre-Python/releases/download/$REL/$value.tar.gz"
-    tar -xzvf $value.tar.gz
-    rm $value.tar.gz
+    ARCH=gempyre-py-$REL-$value.tar.gz
+    wget "https://github.com/mmertama/Gempyre-Python/releases/download/$REL/$ARCH"
+    tar -xzvf $ARCH
+    rm $ARCH
 done
 
 USER=__token__
