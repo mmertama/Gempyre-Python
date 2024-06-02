4ce0f9574cfe6e43ad6619501295fb0bd03b8359
Markus Mertama <mertama@gmail.com>
2024-06-02 17:03:55 +0300

    Update local_run.sh to detect the current OS and python version

--- local_run.sh
+++ local_run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-PYTHONPATH=./_skbuild/macosx-14.0-arm64-3.12/cmake-install/src python3 "$@"
+
+if [ -z ""$@"" ]; then
+    echo "Locally installed (venv?) 'pip3 install -e .' may not find the Gempyre library correctly."
+    echo "This scipts attemps to set the correctly python path for the call. e.g. $(basename $0) my_script.py arg1 arg2..."
+    exit 1
+fi
+
+POTATO=$(python3 -c 'import sysconfig
+platform_info = sysconfig.get_platform()
+python_version = sysconfig.get_python_version()
+print(f"{platform_info}-{python_version}")')
+PYTHONPATH=./_skbuild/$POTATO/cmake-install/src python3 "$@"
 
 
